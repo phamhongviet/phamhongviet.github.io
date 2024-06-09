2bff88fa488edd4b18117f690bb9dd234b08edea
Viet Pham <phamhongviet52@gmail.com>
2024-06-09 13:47:48 +0700

    fix: favicon

--- gen_html.sh
+++ gen_html.sh
@@ -13,13 +13,17 @@ cat > "$html_f" <<EOF
 <head>
 <title>${title}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 <style>
 html {
   margin: 0 auto;
   max-width: 900px;
 }
 body {
-  font-family: Georgia, "Times New Roman", serif;
+  font-family: "Noto Serif", Georgia, "Times New Roman", serif;
 }
 </style>
 </head>
